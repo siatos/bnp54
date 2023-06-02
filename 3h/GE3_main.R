e6e88472238aea1ecc8174767fbe7c00fe8943d3
s3e3 <48084258+siatos@users.noreply.github.com>
2023-06-02 21:48:10 +0000

    Add files via upload

--- 3h/GE3_main.R
+++ 3h/GE3_main.R
@@ -21,37 +21,44 @@ print(paste("in_data - before omit NA vals: ", nrow(in_data), "X", ncol(in_data)
 ##  omit rows from input data having NA values
 ##  na.omit removes ROWs on columns having na values
 ##  so we need to have genes in the ROWs
+
 in_data <- na.omit( as.data.frame(t(in_data)))
 print(paste("in_data - after omit NA vals ", nrow(in_data), "X", ncol(in_data)))
-
 in_data <- as.data.frame(t(in_data))
+
+## save Diagnosis as column
 Diagnosis <- as.matrix(in_data$classes)
 colnames(Diagnosis) <- c("Diagnosis")
+Diagnosis <- as.factor(Diagnosis)
 in_data <- in_data[, -1]
 print(paste("In Data (after removing classes) ", nrow(in_data), "X", ncol(in_data)))
 
+## save gene_ids
 sample_names <- row.names(in_data)
 gene_ids <- as.matrix(colnames(in_data)) 
 
 
 ##  get only numeric
 ## 
-## we need to have in_data as a simple matrix for the function to work
-
+## we need to have in_data as a simple matrix for the tsne function to work
 tsne_data <- as.matrix(sapply(in_data, as.numeric))
 library(Rtsne)
 tsne_out <- Rtsne(tsne_data, perplexity = 20, dim=2) # Run TSNE
-plot(tsne_out$Y[, 1] ~ tsne_out$Y[, 2], pch = 20, col = "black")
+tsne_plot <- data.frame(x = tsne_out$Y[ ,1], y = tsne_out$Y[, 2], col = Diagnosis) 
+ggplot(tsne_plot) + geom_point(aes(x=x, y=y, color=col)) + theme(legend.position="right") + scale_color_manual(values = c("blue", "red"))
+
 
 ## apply correlation 
 ## cor_data matrix is a 8594x8594 matrix
 cor_data <- cor(tsne_data)
-## save corr data into a tmp triangular matrix  set upper part and diagonal to 0
+
+## save corr data into a tmp triangular matrix  (matrix is symmetrical over the diagaonal) 
+## set upper part and diagonal to 0
 tmp <- cor_data
 tmp[upper.tri(tmp)] <- 0
 diag(tmp) <- 0
 
-## exclude columns where correllation > 0.5
+## exclude columns where abs(correllation) > 0.5
 selected_data <- in_data[, !apply(tmp, 2, function(x) any(abs(x) > 0.5, na.rm = TRUE))]
 selected_data_rows <- nrow(selected_data)
 selected_data_cols <- ncol(selected_data)
@@ -62,42 +69,80 @@ print(paste("Selected Data ", nrow(selected_data), "X", ncol(selected_data)))
 
 ## apply Rtsne to the selected of the input data (corr <= 0.5)
 tsne_out_corr <- Rtsne(selected_data, perplexity = 20, dim=2) # Run TSNE
-plot(tsne_out$Y[, 1] ~ tsne_out$Y[, 2], pch = 20, col = "red")
+tsne_plot <- data.frame(x = tsne_out_corr$Y[,1], y = tsne_out_corr$Y[,2], col = Diagnosis) 
+ggplot(tsne_plot) + geom_point(aes(x=x, y=y, color=col)) + theme(legend.position="right") + scale_color_manual(values = c("black", "yellow"))
+
+
+####################################################
+
+
+## Thema 2
+set.seed(1) 
+## scale selected data (66x8459)
+## 
+scaled_data <- sapply(in_data, as.numeric)
+scaled_data <- scale(scaled_data)
+rownames(scaled_data) <- row.names(in_data)
+print(paste("scaled selected data, rows:", nrow(scaled_data), " columns:", ncol(scaled_data)))
+
+print("Find best kmeans cluster separation  k = 1..12 scaled selected data")
+scaled_result <- factoextra::fviz_nbclust(scaled_data, kmeans, method = "wss", k.max = 12)
+scaled_result
+scaled_result$data
+
+## run k-means for scale data & cluster centers
+km_selected_scaled <- kmeans(scaled_data, centers = 6, nstart = 25)
+## display clusters
+clusters_inf_scaled <- factoextra::fviz_cluster(km_selected_scaled, data = scaled_data)
+clusters_inf_scaled
+
+
+##
+# Dissimilarity matrix
+d <- dist(scaled_data, method = "euclidean")
+# Hierarchical clustering using single Linkage
+hc1 <- hclust(d, method = "single" )
+
+# Plot the obtained dendrogram
+plot(hc1, cex = 0.6, hang = -1,  main="Dendrogram Scaled Data - samples 66x8594")
+
+scaled_tsne <- as.data.frame(scale(tsne_out$Y))
+rownames(scaled_tsne) <- row.names(in_data)
+factoextra::fviz_nbclust(scaled_tsne, kmeans, method = "wss", k.max = 12)
+km_sne <- kmeans(scaled_tsne, centers = 5, nstart = 25)
+factoextra::fviz_cluster(km_sne, data = scaled_tsne)
+
+d <- dist(scaled_data, method = "euclidean")
+hc1 <- hclust(d, method = "single" )
+
+# Plot the obtained dendrogram
+plot(hc1, cex = 0.6, hang = -1, main="Dendrogram sne Data -samples 66 x 2")
 
 
 
+## Thema 3
+## Set RFE control
 
-# Thema 3
-# need to install
-#BiocManager::install("randomForest")
-# Set RFE control
 library(caret)
 data <- in_data
 data[] <- lapply(data, as.numeric)  ## convert to numeric
 data <- as.data.frame(data)
-Diagnosis <- as.factor(Diagnosis)
+# Diagnosis <- as.factor(Diagnosis)
 data <- cbind(data, Diagnosis)
 
-#ctrl = rfeControl(functions = rfFuncs, # "rfFuncs" are built-in to caret
-#                  method = "repeatedcv", repeats = 10,
-#                  saveDetails = TRUE)
-ctrl = rfeControl(functions = rfFuncs,
+ctrl = rfeControl(functions = rfFuncs, # "rfFuncs" are built-in to caret
+                  method = "repeatedcv", repeats = 10,
                   saveDetails = TRUE)
-# By using rfFuncs, caret will use a random forest to evaluate the usefulness of a feature.
+
+# By using rfFuncs, caret will use a random forest to evaluate features
 # Set a sequence sizes to search
+
 sizes=c(50, 75, 100, 150, 200, 300, 400, 500, 600, 1000)
-#sizes <- c(50, 200, 300, 400, 1000)
 
 # Use caret's rfe function to fit RF models to these different feature spaces
 rfeResults = rfe(x = data[,1:ncol(data)-1], y = data[,ncol(data)],
                  sizes = sizes,
                  rfeControl = ctrl)
-
-
-#rfeResults = rfe(x = select(nonColinearData, -Depressiv), y = nonColinearData$Depressiv,
-#                 sizes = sizes,
-#                 rfeControl = ctrl)
-
 
 rfeResults$results
 #dev.off()
@@ -108,24 +153,12 @@ X <- rfeResults$variables
 Y <- X[X$Variables == 1000, ]
 Y_1000 <- aggregate(Y[, c("Overall")], list(Y$var), mean)
 Y_1000 <- order(Y_1000[, c("x")], decreasing = TRUE)[1:1000]
+selected_1000 <- in_data[, Y_1000]
 
-Z <- rfeResults[['optVariables']]
-predictors(rfeResults)
+tsne_1000 <- Rtsne(selected_1000, perplexity = 20, dim=2) # Run TSNE
+tsne_plot <- data.frame(x = tsne_1000$Y[,1], y = tsne_1000$Y[,2], col = Diagnosis) 
+ggplot(tsne_plot) + geom_point(aes(x=x, y=y, color=col)) + theme(legend.position="right") + scale_color_manual(values = c("green", "blue"))
 
-
-#set.size = 10 #you want set-size of 10
-#lm.vars <- lmProfile$variables 
-#lm.set <- lm.vars[lm.vars$Variables==set.size,  ] # selects variables of set-size (= 10 here)
-#use aggregate to calculate mean ranking score (under column "Overall")
-lm.set <- aggregate(lm.set[, c("Overall")], list(lm.set$var), mean)
-
-#order from highest to low, and select first 10:
-lm.order <- order(lm.set[, c("x")], decreasing = TRUE)[1:set.size]
-lm.set[lm.order, ]
-
-
-#f = rfeResults.get_support(1) #the most important features
-#X = data[data.columns[f]]
 
 
 ## Thema 4
@@ -195,50 +228,6 @@ boxplot(algorithms,
 
         
 
-####################################################
-
-
-## Thema 2
-set.seed(1) 
-## scale selected data (66x8459)
-## 
-scaled_data <- sapply(in_data, as.numeric)
-scaled_data <- scale(scaled_data)
-rownames(scaled_data) <- row.names(in_data)
-print(paste("scaled selected data, rows:", nrow(scaled_data), " columns:", ncol(scaled_data)))
-
-print("Find best kmeans cluster separation  k = 1..12 scaled selected data")
-scaled_result <- factoextra::fviz_nbclust(scaled_data, kmeans, method = "wss", k.max = 12)
-scaled_result
-scaled_result$data
-
-## run k-means for scale data & cluster centers
-km_selected_scaled <- kmeans(scaled_data, centers = 6, nstart = 25)
-## display clusters
-clusters_inf_scaled <- factoextra::fviz_cluster(km_selected_scaled, data = scaled_data)
-clusters_inf_scaled
-
-
-##
-# Dissimilarity matrix
-d <- dist(scaled_data, method = "euclidean")
-# Hierarchical clustering using single Linkage
-hc1 <- hclust(d, method = "single" )
-
-# Plot the obtained dendrogram
-plot(hc1, cex = 0.6, hang = -1,  main="Dendrogram Scaled Data - samples 66x8594")
-
-scaled_tsne <- as.data.frame(scale(tsne_out$Y))
-rownames(scaled_tsne) <- row.names(in_data)
-factoextra::fviz_nbclust(scaled_tsne, kmeans, method = "wss", k.max = 12)
-km_sne <- kmeans(scaled_tsne, centers = 5, nstart = 25)
-factoextra::fviz_cluster(km_sne, data = scaled_tsne)
-
-d <- dist(scaled_data, method = "euclidean")
-hc1 <- hclust(d, method = "single" )
-
-# Plot the obtained dendrogram
-plot(hc1, cex = 0.6, hang = -1, main="Dendrogram sne Data -samples 66 x 2")
 
 
 
